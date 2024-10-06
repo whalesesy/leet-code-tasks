cf21413e924140f749f64125f68278493eb53b93
celestine nyoka bwari <165144529+whalesesy@users.noreply.github.com>
2024-10-06 16:10:37 +0300

    Time: 0 ms (100%), Space: 10.1 MB (41.45%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,22 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ void preOrder(TreeNode* root , vector<int> &ans)
+ {
+     if(root == NULL)
+     {
+         return ; 
+     }
+
+     ans.push_back(root->val);
+     preOrder(root->left , ans);
+     preOrder(root->right , ans);
+ }
+
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        stack<TreeNode*> st;
-        vector<int> ans;
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+       vector<int> ans;
+       preOrder(root , ans);
 
-        while(!st.empty() || root){
-            while(root){
-                ans.push_back(root->val);
-                st.push(root);
-                root=root->left;
-            }
-            root=st.top();
-            st.pop();
-            root=root->right;
-        }
-        return ans ;
+       return ans; 
     }
 };
