b815d86b6521349751cc79c5788a39391c7461cd
celestine nyoka bwari <165144529+whalesesy@users.noreply.github.com>
2024-10-23 17:15:10 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -22,42 +22,8 @@ public:
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        unordered_map<Node*,Node*>mp;
-        unordered_map<Node*,int>m;
-        stack<Node*>s;
-        s.push(node);
-        Node*ptr=NULL,*p=NULL;
-        while(!s.empty())
-        {
-            ptr=s.top();
-            s.pop();
-            if(ptr==NULL)
-            {
-                break;
-            }
-            if(m[ptr]==0)
-            {
-                mp[ptr]=new Node(ptr->val);
-            }
-            m[ptr]=1;
-            int d=ptr->val;
-            vector<Node*>v(ptr->neighbors);
-            for(int i=0;i<v.size();i++)
-            {
-                p=v[i];
-                if(m[p]==0)
-                {
-                    mp[p]=new Node(p->val);
-                }
-                (mp[ptr]->neighbors).push_back(mp[p]);
-                if(m[p]!=0)
-                {
-                    continue;
-                }
-                m[p]=1;
-                s.push(p);
-            }
-        }
-        return mp[node];
-    }
+
+      private:
+    std::unordered_map<int, Node*> mp;  
+    
 };
