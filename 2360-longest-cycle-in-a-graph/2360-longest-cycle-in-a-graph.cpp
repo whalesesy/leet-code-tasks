00b83a8e18ff6f35dccca085d0c9d146893efe02
celestine nyoka bwari <165144529+whalesesy@users.noreply.github.com>
2024-10-23 16:48:17 +0300

    Time: 645 ms (36.86%), Space: 135.6 MB (51.07%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void dfs(int node, vector<int>& dist_node, vector<bool> & visited, vector<int>& edges, int distance,int &ans, vector<bool> & extra ){
+        if(node!=-1){
+          if(!visited[node]){
+              visited[node]=true;
+              extra[node]=true;
+              dist_node[node]=distance;
+              dfs(edges[node],dist_node,visited,edges,distance+1,ans,extra);
+          }
+          else if(extra[node])
+               {   
+                   ans = max(ans, distance - dist_node[node]);
+               }   
+               extra[node]  = false;
+        }
+       
+    }
+    int longestCycle(vector<int>& edges) {
+        vector<int> dist_node(edges.size(),0);
+        vector<bool>extra(edges.size(),false);
+        vector<bool>visited(edges.size(),false);
+        int ans=-1;
+        for(int i=0;i<edges.size();i++){
+            if(!visited[i]){
+                dfs(i,dist_node,visited,edges,0,ans,extra);
+            }
+        }
+        return ans;
+    }
+};
